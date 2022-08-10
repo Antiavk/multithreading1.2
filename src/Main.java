2c301ff04326c4235f53db69e752441066221b2d
Antiavk <avk_tp@mail.ru>
2022-08-10 15:19:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+//        ThreadGroup threadGroup = new ThreadGroup("threadGroup");
+//        Thread[] threads = new Thread[4];
+//
+//        for (int i = 0; i < threads.length; i++) {
+//            threads[i] = new MyThread(threadGroup, ("поток " + (i + 1)));
+//            threads[i].start();
+//        }
+//
+//        try {
+//            Thread.sleep(15_000);
+//        } catch (InterruptedException err) {
+//        }
+//        System.out.println("Завершаю все потоки.");
+//        threadGroup.interrupt();
+
+        int nThreads = 4;
+
+        MyCallable myCallable = new MyCallable();
+        // Задача печатает несколько сообщений в консоль и завершаться. Результатом возвращает количество отправленных сообщений.
+
+        List<MyCallable> tasks = new ArrayList<>(); //Коллекция с задачами
+
+        //Создание 4 потоков, каждый из которых имеет свое имя.
+        ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+
+        //Добавляем задачи в коллекцию
+        for (int i = 0; i < nThreads; i++) {
+            tasks.add(myCallable);
+        }
+
+        System.out.println("invokeAny. Кол-во выведенных в консоль сообщений " + executorService.invokeAny(tasks));
+
+        for (Future<Integer> task :
+                executorService.invokeAll(tasks)) {
+            System.out.println("invokeAll. Кол-во выведенных в консоль сообщений " + task.get());
+        }
+
+        System.out.println("Завершаю все потоки.");
+        executorService.shutdown();
+    }
+}
